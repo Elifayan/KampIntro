7eddd71702165547f92a8552eb9ccc69f5cf5c29
Elif Ayan <77552697+Elifayan@users.noreply.github.com>
2021-01-24 00:24:56 +0300

    20/01/2021 Kodlama Eğitim Kodları

---
+++ OOP2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Elif Ayan
+            //////Gerçek Müşteri - Tüzel Müşteri(şirket)
+            ///SOLID
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Elif";
+            musteri1.Soyadi = "Ayan";
+            musteri1.TcNo = "12345678910";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(musteri1);
+            customerManager.Ekle(musteri2);
+
+            
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatebaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, new DatebaseLoggerService());
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager ,tasıtKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
